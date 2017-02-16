6138f191d1f320ab2113efb9401aa1c0da07d349
Pål Grønås Drange <pgdr@statoil.com>
2017-02-16 23:45:19 +0100

    added /etc/os-release to sysinfo

--- tests/test_fby_runner.py
+++ tests/test_fby_runner.py
@@ -10,4 +10,9 @@ class FbyRunnerTest(TestCase):
 
     def test_load(self):
         fby_client = main_module.FbyRunner( ["arg1","arg2"] )
-        
+
+    def test_sysinfo(self):
+        sys_info = main_module.get_sys_info()
+        self.assertTrue('sysname'  in sys_info)
+        self.assertTrue('requests' in sys_info)
+        self.assertTrue('python'   in sys_info)
