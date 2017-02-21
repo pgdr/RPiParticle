dcd904c065e61faaf23f6f2da6383d47d45bce72
Pål Grønås Drange <pgdr@statoil.com>
2017-02-21 23:07:45 +0100

    fixed libs url to submitter, test, var_path, logging

--- lib/dist.py
+++ lib/dist.py
@@ -7,7 +7,7 @@ files = ["bin/fby_client",
          "lib/sds011.py",
          "lib/friskby_client.py",
          "lib/friskby_dao.py",
-         "lib/friskby_submitter.py",
+         "lib/fby_submitter.py",
          "lib/os_release.py"]
 
 directories = ["var",
--- lib/fby_submitter.py
+++ lib/fby_submitter.py
@@ -1,17 +1,25 @@
+import sys
 import json
 import requests
 
 class FriskbySubmitter(object):
     """This class submits data from a database to the friskby cloud, then proceeds
-    to mark the uploaded data as such.  The constructor takes a device_config
-    (to read https keys and urls) and a path to an sqlite file.
+    to mark the uploaded data as such.
+
     """
 
-    def __init__(self, device_config, dao):
-        self.device_config = device_config
+    def __init__(self, dao):
         self.dao = dao
 
+    def set_config(self, device_config):
+        self.device_config = device_config
+
+    def get_dao(self):
+        return self.dao
+
     def _upload(self, rows):
+        print('Attempting to upload.')
+        sys.stdout.flush()
         # id, value, sensor, timestamp, uploaded
         data = {}
         for row in rows:
@@ -20,24 +28,37 @@ class FriskbySubmitter(object):
                 data[sensor] = []
             data[sensor].append((id_, value, sensor, timestamp))
 
+        print('Connecting.')
+        sys.stdout.flush()
         for sensor in data:
-            push = {"sensorid"   : sensor,
-                    "value_list" : data[sensor],
+            sensor_id = self.device_config.getSensorId(sensor)
+            push = {"sensorid"   : sensor_id,
+                    "value_list" : [(x[3], x[1]) for x in data[sensor]], # (time, val)
                     "key"        : self.device_config.getPostKey()}
+            print('Posting to %s' % self.device_config.getPostURL())
+            print(push)
+            sys.stdout.flush()
             respons = requests.post(self.device_config.getPostURL(),
                                     headers={'Content-Type': 'application/json'},
                                     data=json.dumps(push),
                                     timeout=30)
-        respons.connection.close()
-        if respons.status_code != 201:
-            respons.raise_for_status()
-            raise Exception('Server did not respond with 201 Created.  Response: %d %s'
-                            % (respons.status_code, respons.text))
+            print('posted %s' % sensor)
+            sys.stdout.flush()
+            if respons.status_code != 201:
+                respons.raise_for_status()
+                raise Exception('Server did not respond with 201 Created.  Response: %d %s'
+                                % (respons.status_code, respons.text))
+            respons.connection.close()
         return True # no exception, everything written
 
     def post(self):
+        if self.device_config is None:
+            raise ValueError('Device config not set!')
         to_upload = self.dao.get_non_uploaded()
-        if len(to_upload) <= 4:
+        if len(to_upload) < 10:
             return
+
+        print('Submitting ...')
+        sys.stdout.flush()
         if self._upload(to_upload):
             self.dao.mark_uploaded(to_upload)
