988290dfe075aae76f795aaf0e23394a09ef8ef7
Pål Grønås Drange <pgdr@statoil.com>
2017-02-22 18:12:00 +0100

    added dao.get_num_rows method

--- tests/test_dao.py
+++ tests/test_dao.py
@@ -1,18 +1,16 @@
 from tempfile import NamedTemporaryFile as temp
-from datetime import datetime as dt
 from unittest import TestCase
-import os.path
-from ts import TS
-from friskby_dao import FriskbyDao
 from random import random as rnd
 import sqlite3
+from ts import TS
+from friskby_dao import FriskbyDao
 
 def rand():
     return round(rnd()*100, 2)
 
 def gen_rand_ts():
     t = TS()
-    for i in range(3):
+    for _ in range(3):
         t.append(rand())
     return t
 
@@ -32,25 +30,42 @@ class DaoTest(TestCase):
         self.assertEqual(1, len(result))
         self.assertEqual('samples', result[0][0])
 
+    def _do_test_num_upl(self, dao, exp_num_all, exp_num_upl, exp_num_nup):
+        num_all, num_upl, num_nup = (dao.get_num_rows(),
+                                     dao.get_num_rows(uploaded_status=True),
+                                     dao.get_num_rows(uploaded_status=False))
+        self.assertEqual(exp_num_all, num_all)
+        self.assertEqual(exp_num_upl, num_upl)
+        self.assertEqual(exp_num_nup, num_nup)
+
     def test_persist(self):
         num_data = 13
-        for i in range(num_data):
+        for _ in range(num_data):
             t10 = gen_rand_ts()
             t25 = gen_rand_ts()
-            self.dao.persist_ts((t10,t25))
+            self.dao.persist_ts((t10, t25))
+
+        self._do_test_num_upl(self.dao, 2*num_data, 0, 2*num_data)
+
         data = self.dao.get_non_uploaded(limit=30)
         self.assertEqual(2*num_data, len(data))
 
     def test_mark_uploaded(self):
         num_data = 17
-        for i in range(num_data):
+        for _ in range(num_data):
             t10 = gen_rand_ts()
             t25 = gen_rand_ts()
-            self.dao.persist_ts((t10,t25))
+            self.dao.persist_ts((t10, t25))
         print(repr(self.dao))
+
+        self._do_test_num_upl(self.dao, 2*num_data, 0, 2*num_data)
+
         data = self.dao.get_non_uploaded(limit=30)
         self.assertEqual(30, len(data))
         self.dao.mark_uploaded(data)
         data = self.dao.get_non_uploaded(limit=30)
         self.assertEqual(4, len(data)) # total 34, marked 30
         print(repr(self.dao))
+
+        # test num / num upl / num non-upl
+        self._do_test_num_upl(self.dao, 2*num_data, 30, 2*num_data - 30)
